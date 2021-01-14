cc17fbe1dea720988463d4da1bff7d1ee7bedb26
Brendan Chou <brendanchou@msn.com>
2021-01-14 14:34:32 -0800

    v0.5.0: Use newest Ropsten exchange address (#21)
    * update ropsten address again
    
    * 0.5.0
    
    * add test
    
    * 251 bits fix

--- src/lib/BytesHelper.ts
+++ src/lib/BytesHelper.ts
@@ -1,3 +1,10 @@
+import BigNumber from 'bignumber.js';
+import Web3 from 'web3';
+
+import {
+  BigNumberable,
+} from '../types';
+
 export function stripHexPrefix(input: string) {
   if (input.startsWith('0x')) {
     return input.slice(2);
@@ -11,4 +18,31 @@ export function addressesAreEqual(
 ): boolean {
   return !!(addressOne && addressTwo &&
     (stripHexPrefix(addressOne).toLowerCase() === stripHexPrefix(addressTwo).toLowerCase()));
+}
+
+const ASSET_ID_MASK = new BigNumber(2).pow(251);
+
+export function getAssetId(
+  tokenAddress: string,
+  quantization: BigNumberable = 1,
+): string {
+  const tokenHash: string = soliditySha3('ERC20Token(address)').substr(0, 10);
+  const resultBytes = soliditySha3(
+    { type: 'bytes4', value: tokenHash },
+    { type: 'uint256', value: tokenAddress },
+    { type: 'uint256', value: new BigNumber(quantization).toFixed(0) },
+  );
+  const resultBN = new BigNumber(resultBytes, 16);
+  const result = resultBN.mod(ASSET_ID_MASK);
+  return `0x${result.toString(16).padStart(64, '0')}`;
+}
+
+function soliditySha3(
+  ...input: any
+): string {
+  const result = Web3.utils.soliditySha3(...input);
+  if (!result) {
+    throw new Error('soliditySha3 produced null output');
+  }
+  return result;
 }
