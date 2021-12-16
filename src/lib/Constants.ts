177a1c647b0709543c8ce453bb527e77d1a27056
Sam Weinberg <samweinberg23@gmail.com>
2021-12-16 11:12:26 -0800

    update usdcExContract address

--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -17,13 +17,13 @@ export const COLLATERAL_ASSET_ID = {
   [Networks.ROPSTEN]: getAssetId(usdcAbi.networks[3].address),
 };
 
-// TODO fix zero ex addresses and contract abi addresses for proxy-deposit
+// TODO fix usdc ex address for PROD and contract abi addresses for proxy-deposit
 export const ADDRESSES = {
   ZERO: '0x0000000000000000000000000000000000000000',
   DYDX_USDC_ADDRESS_ROPSTEN: '0x8707a5bf4c2842d46b31a405ba41b858c0f876c4',
   USDC_ADDRESS_MAINNET: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  ZERO_EX_EXCHANGE_ADDRESS_ROPSTEN: '0x7e64a9800439efc627ce0fae88963397d21ad3ac',
-  ZERO_EX_EXCHANGE_ADDRESS_MAINNET: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  USDC_EXCHANGE_ADDRESS_ROPSTEN: '0x144b40e133495811e1d5fd64ab73ed40186ecb17',
+  USDC_EXCHANGE_ADDRESS_MAINNET: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
 };
 
 export const USDC_ADDRESSES: {[networkId: number]: string } = {
@@ -31,7 +31,7 @@ export const USDC_ADDRESSES: {[networkId: number]: string } = {
   [Networks.MAINNET]: ADDRESSES.USDC_ADDRESS_MAINNET,
 };
 
-export const ZERO_EX_EXCHANGE_ADDRESSES: {[networkId: number]: string} = {
-  [Networks.ROPSTEN]: ADDRESSES.ZERO_EX_EXCHANGE_ADDRESS_ROPSTEN,
-  [Networks.MAINNET]: ADDRESSES.ZERO_EX_EXCHANGE_ADDRESS_MAINNET,
+export const USDC_EXCHANGE_ADDRESSES: {[networkId: number]: string} = {
+  [Networks.ROPSTEN]: ADDRESSES.USDC_EXCHANGE_ADDRESS_ROPSTEN,
+  [Networks.MAINNET]: ADDRESSES.USDC_EXCHANGE_ADDRESS_MAINNET,
 };
