84c33e5cf600ebab7babaf8911f763ef5e28caed
James Jia <james@dydx.exchange>
2022-09-01 15:10:11 -0700

    v0.14.7 Add Goerli

--- __tests__/StarkwareLib.test.ts
+++ __tests__/StarkwareLib.test.ts
@@ -1,9 +1,10 @@
+import Web3 from 'web3';
 import { provider as Provider } from 'web3-core';
 
+import usdcAbi from '../src/contracts/usdc-abi.json';
 import { StarkwareLib } from '../src/index';
-import { DummyProvider } from './helpers/DummyProvider';
 import { Networks } from '../src/types';
-import Web3 from 'web3';
+import { DummyProvider } from './helpers/DummyProvider';
 
 // async ropsten calls can take >10k MS
 jest.setTimeout(40000);
@@ -12,7 +13,7 @@ describe('StarwareLib#initiate', () => {
   const dummyProvider = new DummyProvider() as unknown as Provider;
 
   it('Successfully initiates StarkwareLib on Ropsten', async () => {
-    const starkwareLib = new StarkwareLib(dummyProvider, Networks.ROPSTEN);
+    const starkwareLib = new StarkwareLib(dummyProvider, Networks.GOERLI);
     expect(starkwareLib.collateralToken).toBeTruthy();
     expect(starkwareLib.contracts).toBeTruthy();
     expect(starkwareLib.exchange).toBeTruthy();
@@ -26,20 +27,20 @@ describe('StarwareLib#initiate', () => {
         { timeout: 10000 },
       );
 
-      const real = new StarkwareLib(realProvider, Networks.ROPSTEN);
+      const real = new StarkwareLib(realProvider, Networks.GOERLI);
       const account = real.web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY as string);
       real.web3.eth.accounts.wallet.add(account);
       real.web3.eth.defaultAccount = account.address;
 
       await real.exchange.setERC20Allowance({
-        tokenAddress: '0x8707a5bf4c2842d46b31a405ba41b858c0f876c4',
+        tokenAddress: usdcAbi.networks[5].address,
         address: '0x029bB89d64695D6A461eEbC1Aab4a4C8657a3f22',
         amount: 100,
       }, { from: account.address });
 
       const result: string = await real.exchange.getERC20Allowance({
         ownerAddress: account.address,
-        tokenAddress: '0x8707a5bf4c2842d46b31a405ba41b858c0f876c4',
+        tokenAddress: usdcAbi.networks[5].address,
         spenderAddress: '0x029bB89d64695D6A461eEbC1Aab4a4C8657a3f22',
         decimals: 8,
       });
